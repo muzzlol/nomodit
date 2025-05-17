72e0bcda4eb04aef8fc69a8bc06b575b993bfa29
muzzlol <muzxmmilkhxn@gmail.com>
2025-05-18 05:26:16 +0530

    hlep

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -3,132 +3,138 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/spinner"
-	"github.com/charmbracelet/bubbles/textarea"
-	"github.com/charmbracelet/bubbles/viewport"
+	// "github.com/charmbracelet/bubbles/cursor"
+	// "github.com/charmbracelet/bubbles/textinput"
+
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-const gap = "\n\n"
+const (
+	gap   = "\n\n"
+	title = `
+  /|/_ _ _ _ _/._/_
+ / /_/ / /_/_// /
+	`
+)
+
+/**
+	title1 = `
+  ┳┓        ┓•
+  ┃┃┏┓┏┳┓┏┓┏┫┓╋
+  ┛┗┗┛┛┗┗┗┛┗┻┗┗
+
+`
+	title = `
+	▖ ▖        ▌▘▗
+	▛▖▌▛▌▛▛▌▛▌▛▌▌▜▘
+	▌▝▌▙▌▌▌▌▙▌▙▌▌▐▖
+
+`
+*/
 
 var (
 	focusedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("205")) // 205 - Hot Pink
 	blurredStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("240")) // 240 - Gray
 	cursorStyle  = focusedStyle
-	helpStyle    = blurredStyle
+	accentStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("34"))         // 34 - Dark Cyan/Green
 	textStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("252")).Render // 252 - Light Gray
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("34"))         // 34 - Dark Cyan/Green
 
-	loadSpinner       = spinner.Pulse
-	generatingSpinner = spinner.Points
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
 )
 
 func main() {
 	p := tea.NewProgram(initialModel())
-
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 type model struct {
-	spinner     spinner.Spinner
-	viewport    viewport.Model
-	messages    []string
-	textarea    textarea.Model
-	senderStyle lipgloss.Style
-	err         error
+	title        string
+	currentState state
+	cursor       int
+	submit       string
+	help         help.Model
+	keys         keyMap
+}
+
+type keyMap struct {
+	Navigation key.Binding
+	Submit     key.Binding
+	Quit       key.Binding
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Navigation, k.Submit, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Navigation, k.Submit, k.Quit},
+	}
+}
+
+var keys = keyMap{
+	Navigation: key.NewBinding(
+		key.WithKeys("tab", "shift+tab"),
+		key.WithHelp("tab/shift+tab", "navigate"),
+	),
+	Submit: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "submit"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c", "esc"),
+		key.WithHelp("ctrl+c/esc", "quit"),
+	),
+}
+
+type state struct {
+	text    string
+	spinner spinner.Spinner
 }
 
 func initialModel() model {
-	ta := textarea.New()
-	ta.Placeholder = "Send a message..."
-	ta.Focus()
-
-	ta.Prompt = "┃ "
-
-	ta.SetWidth(30)
-	ta.SetHeight(3)
-
-	// Remove cursor line styling
-	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
-
-	ta.ShowLineNumbers = false
-
-	vp := viewport.New(30, 5)
-	vp.SetContent(`Welcome to the chat room!
-Type a message and press Enter to send.`)
-
-	ta.KeyMap.InsertNewline.SetEnabled(false)
 
 	return model{
-		textarea:    ta,
-		messages:    []string{},
-		viewport:    vp,
-		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
-		err:         nil,
+		title: accentStyle.Render(title),
+		currentState: state{
+			text:    textStyle(""),
+			spinner: spinner.Pulse,
+		},
+		cursor: 1, // instructions part
+		keys:   keys,
 	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textarea.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		tiCmd tea.Cmd
-		vpCmd tea.Cmd
-	)
-
-	m.textarea, tiCmd = m.textarea.Update(msg)
-	m.viewport, vpCmd = m.viewport.Update(msg)
-
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.viewport.Width = msg.Width
-		m.textarea.SetWidth(msg.Width)
-		m.viewport.Height = msg.Height - m.textarea.Height() - lipgloss.Height(gap)
-
-		if len(m.messages) > 0 {
-			// Wrap content before setting it.
-			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
-		}
-		m.viewport.GotoBottom()
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC, tea.KeyEsc:
-			fmt.Println(m.textarea.Value())
+		switch {
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
-		case tea.KeyEnter:
-			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
-			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
-			m.textarea.Reset()
-			m.viewport.GotoBottom()
+		case key.Matches(msg, m.keys.Navigation):
+			m.cursor++
+		case key.Matches(msg, m.keys.Submit):
+			m.cursor--
 		}
-
-	// We handle errors just like any other message
-	case error:
-		m.err = msg
-		return m, nil
 	}
 
-	return m, tea.Batch(tiCmd, vpCmd)
-}
-
-func (m model) startSpinner(s spinner.Spinner) {
-	m.spinner = spinner.New()
-	m.spinner.Style = spinnerStyle
-	m.spinner.Spinner = s
+	return m, nil
 }
 
 func (m model) View() string {
-	return fmt.Sprintf(
-		"%s%s%s",
-		m.viewport.View(),
-		gap,
-		m.textarea.View(),
-	)
+	return m.title + gap + m.currentState.text + gap + m.submit + gap + m.help.View(m.keys)
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
 }
